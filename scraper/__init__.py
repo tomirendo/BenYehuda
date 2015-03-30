1617da1ad831da37911e54cfae9a99907f91acfb
nice-shot <shefferu@gmail.com>
2015-03-30 22:13:18 +0300

    added the super basic building blocks for the scraper script. Next part is adding Ido's magic

---
+++ scraper/__init__.py
@@ -0,0 +1,58 @@
+
+from bs4 import BeautifulSoup
+
+class Chapter(object):
+    """
+    Chapters in a piece. The main text lives here
+    """
+    def __init__(self, name, index, data):
+        self.name = name
+        self.index = index
+        self.data = data
+
+
+class Piece(object):
+    """
+    Holds the piece's name, url and chapters
+    """
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.chapters = []
+        self.scrape()
+
+    def scrape(self):
+        """
+        Loads the page and gets the chapters
+        """
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page)
+        ### Do something smart to collect chapters from page
+
+class Creator(object):
+    """
+    Holds the creator's name and url
+    """
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def get_pieces(self):
+        raise NotImplementedError()
+
+class BenYehuda(object):
+    """
+    Main class for the BenYehuda project scraper
+    """
+    def get_news(self):
+        return []
+
+    def get_pieces(self):
+        return []
+
+    def get_creators(self):
+        """
+        :return: A list of all the site's creators
+        :rtype: list[Creator]
+        """
+        raise NotImplementedError()
