2c07fb4505e2ebaeac8c2cfc337eb856b15cf0bd
Yotam Vaknin <tomirendo@gmail.com>
2015-05-02 20:29:25 +0300

    Added nikudFunctions.py to main

---
+++ main/NikudFunctions.py
@@ -0,0 +1,32 @@
+def remove_nikud(txt,encoding="utf8"):
+    """
+        Removes nikud from a text
+        Returns the texts in the given encoding. 
+        If txt is unicode -> returns unicode object. 
+        Python2\3 compatible
+    """
+    alef = 1488 
+    min_nikud = 1416 
+    max_nikud = 1479
+    
+    try :
+        nikud_chars = [unichr(i) for i in range(min_nikud,alef)]
+        non_unicode_type = str
+    except NameError :#Python3
+        nikud_chars = [chr(i) for i in range(min_nikud,alef)]
+        non_unicode_type = bytes
+        
+    if not isinstance(txt,non_unicode_type):
+        was_unicode = True
+        unicode_txt = txt
+    else :
+        was_unicode = False
+        unicode_txt = txt.decode(encoding)
+    
+    for char in nikud_chars:
+        unicode_txt = unicode_txt.replace(char,u"")
+    
+    if not was_unicode:
+        return unicode_txt.encode(encoding)
+    else :
+        return unicode_txt
