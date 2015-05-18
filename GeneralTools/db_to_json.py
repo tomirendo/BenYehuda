96523aa4b92ca5a2b7a74217e02e05c5a20831e1
yotam <tomirendo@gmail.com>
2015-05-18 19:42:17 +0300

    Removed data

--- GeneralTools/db_to_json.py
+++ GeneralTools/db_to_json.py
@@ -2,7 +2,13 @@ from main import models
 from json import dumps
 
 ps = models.Piece.objects.all()
+cts = models.Creator.objects.all()
 
 pieces = [i.to_dict() for i in ps]
 
-print(dumps(pieces))
+creators = [i.to_dict() for i in cts]
+
+data_file = "data/data_file.json"
+with open(data_file,"w") as f:
+	f.write(dumps(pieces + creators))
+
