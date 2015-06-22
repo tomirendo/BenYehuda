9a05f5e0b62d6033f9c752a02b3ec73653331897
yotam <tomirendo@gmail.com>
2015-06-22 21:14:37 +0300

    elastic bugs

--- data/send_data_to_elasticsearch.py
+++ data/send_data_to_elasticsearch.py
@@ -8,23 +8,26 @@ class url(str):
             addition = '/' + addition
         return url(self + str(addition) + '/')
     def to_url(self):
-        return urllib.request.quote(self)
+	begin,*end = self.split('?')
+        final = begin +'?'+ '?'.join(map(urllib.request,quote(end))
+        return final
 
 def encoded_json_for_object(obj):
-    return obj.to_dict.encode('utf8')
+    return json.dumps(obj.to_dict()).encode('utf8')
 
 index_url = url('http://localhost:9200/benyehuda/')
 
 def send_collection(collection,object_name):
     url  = index_url.add_path(object_name)
-    for obj,idx in zip(colleciton,count(1)):
+    for obj,idx in zip(collection,count(1)):
         data = encoded_json_for_object(obj)
         req =  urllib.request.Request( url.add_path(count).to_url(),data,method='PUT')
         print(urllib.request.urlopen(req).read())
+    print idx
 
 
 def collection_from_model(model):
     return model.objects.all()
     
-send_collection(collection_from_model(model.Creator),'creator')
-send_collection(collection_from_model(model.Piece),'piece')
+send_collection(collection_from_model(models.Creator),'creator')
+send_collection(collection_from_model(models.Piece),'piece')
