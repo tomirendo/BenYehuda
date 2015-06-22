434584483519c32b48689ccd9a4847ac7d7aabec
Yotam Vaknin <tomirendo@gmail.com>
2015-06-22 21:05:30 +0300

    better script to send to elasticsearch

--- GeneralTools/epub_creator.py
+++ GeneralTools/epub_creator.py
@@ -1,42 +1,44 @@
 from main import models
 from ebooklib import epub
 
-book = epub.EpubBook()
+pieces = models.Piece.objects.all()
+for index,piece in enumerate(pieces):
+    book = epub.EpubBook()
 
-# set metadata
-book.set_identifier('id123456')
-book.set_title('Sample book')
-book.set_language('en')
+    # set metadata
+    book.set_title(piece.name)
+    book.set_language('he')
 
-book.add_author('Author Authorowski')
-book.add_author('Danko Bananko', file_as='Gospodin Danko Bananko', role='ill', uid='coauthor')
+    book.add_author(piece.creator.name)
 
-# create chapter
-c1 = epub.EpubHtml(title='Intro', file_name='chap_01.xhtml', lang='hr')
-c1.content=u'<h1>Intro heading</h1><p>Žaba je skočila u baru.</p>'
+    # create chapter
+    spn = []
+    for chapter in sorted(book.chapters.objects.all(),key = lambda x:x.index):
+        c = epub.EpubHtml(title=chapter.name, file_name='{}.xhtml'.format(chapter.index), lang='he')
+        c.content=chapter.text
 
-# add chapter
-book.add_item(c1)
+        book.add_item(c)
+        spn.append(c)
 
-# define Table Of Contents
-book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
-             (epub.Section('Simple book'),
-             (c1, ))
-            )
+    # # define Table Of Contents
+    # book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
+    #              (epub.Section('Simple book'),
+    #              (c1, ))
+    #             )
 
-# add default NCX and Nav file
-book.add_item(epub.EpubNcx())
-book.add_item(epub.EpubNav())
+    # add default NCX and Nav file
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
 
-# define CSS style
-style = 'BODY {color: white;}'
-nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+    # define CSS style
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
 
-# add CSS file
-book.add_item(nav_css)
+    # add CSS file
 
-# basic spine
-book.spine = ['nav', c1]
+    book.add_item(*([nav_css] + spn))
 
-# write to the file
-epub.write_epub('test.epub', book, {})
+    # basic spine
+
+    # write to the file
+    epub.write_epub('{}.epub'.format(index), book, {})
--- data/send_data_to_elasticsearch.py
+++ data/send_data_to_elasticsearch.py
@@ -1,11 +1,24 @@
 from main import models
-import urllib
-import json
+import urllib,json
 from itertools import count
-url = 'http://localhost:9200/benyehuda/piece/'
-for piece,idx in zip(models.Piece.objects.all(),count(1)):
-    data = json.dumps(piece.to_dict()).encode('utf8') 
-    print(data)
-    req = urllib.request.Request(url +str(idx)+'/',data,method='PUT')
-    print(urllib.request.urlopen(req).read())
+
+class url(str):
+    def add_path(self,addition):
+        if self[-1] != '/':
+            addition = '/' + addition
+        return url(self + str(addition) + '/')
+    def to_url(self):
+        return urllib.request.quote(self)
+
+def encoded_json_for_object(obj):
+    return obj.to_dict.encode('utf8')
+
+index_url = url('http://localhost:9200/benyehuda/')
+
+def send_collection(collection,object_name):
+    url  = index_url.add_path(object_name)
+    for obj,idx in zip(colleciton,count(1)):
+        data = encoded_json_for_object(obj)
+        req =  urllib.request.Request( url.add_path(count).to_url(),data,method='PUT')
+        print(urllib.request.urlopen(req).read())
 
