ea0a983c8129d48e794575ee92b3e573c0138a88
Yotam Vaknin <tomirendo@gmail.com>
2015-06-22 22:11:04 +0300

    should be final

--- data/elastic_query_api.py
+++ data/elastic_query_api.py
@@ -1,13 +1,15 @@
-from data.url import url,index_url
+from url import url,index_url
 import urllib,json
 
 def query_url(obj_type,query):
-    return index_url.add_path(obj_type).add_query(query)
+    search_url = '_search'
+    return index_url.add_path(obj_type).add_path(search_url).add_query(query)
 
 def query(url):
-    result = json.loads(urllib.request.urlopen(url.to_url()).read())
-
-nitshe = b'\xd7\xa0\xd7\x99\xd7\x98\xd7\xa9\xd7\x94'.decode('utf8')
-search_url = query_url('creator',nitshe)
-print(query(search_url))
+    return json.loads(urllib.request.urlopen(url.to_url()).read().decode("utf8"))
+    
+if __name__ == '__main__':
+    nitshe = b'\xd7\xa0\xd7\x99\xd7\x98\xd7\xa9\xd7\x94'.decode('utf8')
+    search_url = query_url('creator',nitshe)
+    print(search_url.to_url())
 
--- data/url.py
+++ data/url.py
@@ -1,23 +1,24 @@
+"""
+A simple url object for elasticsearch url's
+Python 3 only  
+"""
 from urllib.request import quote
 
 class url(str):
     def add_path(self,addition):
         """
-            Taking care of trailing '/' of the elastic url path
+        Taking care of trailing '/' of the elastic url path
         """
         if self[-1] != '/':
             addition = '/' + addition
         return url(self + str(addition) + '/')
 
-    def add_query(query):
+    def add_query(self,query):
         return url(self + '?q={}'.format(query))
 
     def to_url(self):
-        """
-            Quotes url in the RIGHT WAY!
-        """
         begin,*end = self.split('?q=')
-        end_str = '?q='.join(urllib.request.quote(p) for p in end)
+        end_str = quote('?q='.join(end))
         final = begin +'?q='+ end_str
         return final
 
