85bbd3591588fa66d4c2082ed77ba90822737de1
yotam <tomirendo@gmail.com>
2015-07-01 22:17:58 +0300

    modified dir

--- scraper/sample_data.py
+++ scraper/sample_data.py
@@ -8,8 +8,9 @@ def hebrew_to_english(text):
     translate = {chr(ord('א')+heb):eng for heb,eng in zip(range(27),"abgdhwzhtycclmmnns@ppzzkkrst")}
     return "".join(translate[i] for i in text if i in translate)
 
+sample_dir = 'sample'
 def write_creator_files(creator):
-    os.mkdir(hebrew_to_english(creator.name))
+    os.mkdir(os.path.join(sample_dir,hebrew_to_english(creator.name)))
     for piece in creator.pieces:
-        with open(os.path.join(hebrew_to_english(creator.name),hebrew_to_english(piece.name),'.json'),"rb") as f:
+        with open(os.path.join(sample_dir,hebrew_to_english(creator.name),hebrew_to_english(piece.name),'.json'),"rb") as f:
             f.write(json.dumps(piece.as_dict()))
