f419af6e7d99c577d525f2abf1ee4c9169b18378
yotam <tomirendo@gmail.com>
2015-07-01 22:17:00 +0300

    create files the right way

--- scraper/sample_data.py
+++ scraper/sample_data.py
@@ -2,11 +2,14 @@ import os
 import json 
 
 from scraper import Creator
-
 creator = Creator('חיים נחמן ביאליק','http://benyehuda.org/bialik/',verbos = True)
 
+def hebrew_to_english(text):
+    translate = {chr(ord('א')+heb):eng for heb,eng in zip(range(27),"abgdhwzhtycclmmnns@ppzzkkrst")}
+    return "".join(translate[i] for i in text if i in translate)
+
 def write_creator_files(creator):
-    os.mkdir(creator.name)
+    os.mkdir(hebrew_to_english(creator.name))
     for piece in creator.pieces:
-        with open(os.path.join(creator.name,piece.name,'.json'),"rb") as f:
+        with open(os.path.join(hebrew_to_english(creator.name),hebrew_to_english(piece.name),'.json'),"rb") as f:
             f.write(json.dumps(piece.as_dict()))
